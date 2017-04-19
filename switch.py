43b81a80370c0fb631b7665680a019aab197965c
Gyhth <Gyhthy@gmail.com>
2017-04-19 10:04:16 -0400

    Code and Schematics
    The code and wiring schematics for the current build. Note: Application
    used to create the Schematics is called Fritzing .
    http://fritzing.org/home/

---
+++ switch.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+from subprocess import call
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        call("shutdown -h now \"Shut down by button.\"", shell=True)
