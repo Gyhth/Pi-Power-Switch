cbad9accb8032ad4a6444e7a792336546944be55
Gyhth <Gyhthy@gmail.com>
2017-04-20 00:09:58 -0400

    Pausing loop
    Was omitted during previous submission, due to testing and forgetting to
    add it back. Now causes a slight pause in the application during each
    loop to ease CPU usage.

--- switch.py
+++ switch.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from subprocess import call
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -8,3 +9,4 @@ while True:
     input_state = GPIO.input(18)
     if input_state == False:
         call("shutdown -h now \"Shut down by button.\"", shell=True)
+		time.sleep(0.2)
